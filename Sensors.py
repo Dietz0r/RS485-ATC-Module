0dc3d1bd449ed712a0d3e88a4dbf72ea370191ee
Dietz0r <102995203+Dietz0r@users.noreply.github.com>
2024-04-24 21:56:17 +0200

    Setup Sensor.Pins Interrup testing

--- Sensors.py
+++ Sensors.py
@@ -1,26 +1,25 @@
 from Settings import SensorPins
 import lib.umodbus.modbus as modbus
-import _thread
+import asyncio
 import time
 
-def SensorControl():
+def SensorSetup():
     def __init__(self):
-        if SensorPins.PullUpDown == "Up":
+        # set up weak internal pull_up or pull_down resistors
+        if SensorPins.PullUpDown == 'Up':
             for i in range(SensorPins.SensorNum):
-                SensorPins.Sensor[i].init(SensorPins.Sensor[i].PULL_UP)
+                SensorPins.Sensor[i+1].init(SensorPins.Sensor[i+1].PULL_UP)
                 print(f'Setting Sensorpins to internal Pull_Up mode')
-        elif SensorPins.PullUpDown == "Down":
+        elif SensorPins.PullUpDown == 'Down':
             for i in range(SensorPins.SensorNum):
-                SensorPins.Sensor[i].init(SensorPins.Sensor[i].PULL_DOWN)
+                SensorPins.Sensor[i+1].init(SensorPins.Sensor[i+1].PULL_DOWN)
                 print(f'Setting Sensorpins to internal Pull_Down mode')
         else:
             print(f'Leaving SensorPins in default state.')
+        
+        # set up pin change interrupt functions
+        for i in range(SensorPins.SensorNum):
+            SensorPins.Sensor[i+1].irq(trigger = SensorPins.Sensor[i+1].IRQ_HIGH_LEVEL | SensorPins.Sensor[i+1].IRQ_LOW_LEVEL, handler=set_register)
 
-delay = (SensorPins.Timing / 1000)    # delay in ms
-
-def sensor_thread(delay, id):
-    while True:
-        time.sleep(delay)
-        print(f'Running sensor thread {id} every {delay}ms')
-
-_thread.start_new_thread(sensor_thread, (delay, 1))
+def set_register(Pin):
+    print(f'Input pin level change: Pin{Pin}')
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 ## Main.py for RS485 extender board
 import RGBLed
 RGBLed.setRGBsolid(100, 0, 0)
-print("Starting RS485...")
+print('Sensors Setup...')
+import Sensors
+RGBLed.setRGBsolid(100, 100, 0)
+print('Starting RS485...')
 import RS485
 RGBLed.setRGBsolid(0, 100, 0)
